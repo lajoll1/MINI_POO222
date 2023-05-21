5897bec88f8ce1d2420c664afdfe8663fa6157db
33Onyx33 <onyxcroft8@gmail.com>
2023-05-21 17:10:33 +0200

    Classes_Art/Post/Seq
    J'ai continué l'élaboration des trois classes. Je suis à quelques problèmes d'avoir tout fini

--- Articulation_Posture_Sequence.py
+++ Articulation_Posture_Sequence.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 import math
-
+import numpy as np
 '________________Ouverture_fichier_XML________________'
 
 xml_file = "/Users/virgilejamot/Documents/GitHub/MINI_POO222/Postures_captures.xml"
@@ -13,33 +13,50 @@ tronc = arbreXML.getroot()
 '____________Definition_classe_Articulation___________'
 
 class Articulation():
-    def __init__(self,tonnom,taposition):
+    def __init__(self,tonnom,taposition,taposture,tonvoisindavant,tesvoisinsdapresXML):
         self._tonnom = str(tonnom)
         self._taposition = [float(num) for num in taposition.strip('()').split(',')]
+        self._taposture = int(taposture)
+        self._voisinsXML = {"P":tonvoisindavant,"N":tesvoisinsdapresXML}
+        
         
     def _lire_nom(self):
         return self._tonnom
     def _lire_position(self):
         return self._taposition
+    def _lire_posture(self):
+        return self._taposture
+    def _lire_voisinsXML(self):
+        return self._voisinsXML
     
     nom = property(_lire_nom)
     position = property(_lire_position)
-    
-    def trouver_voisins(self):
-        index = Artics.index(self)
-        neighbors = []
-        if index > 0:
-            neighbors.append(Artics[index-1])
-        if index < len(Artics) - 1:
-            neighbors.append(Artics[index+1])
-        return neighbors
-    voisins = property(trouver_voisins)
+    posture = property(_lire_posture)
+    voisinsXML = property(_lire_voisinsXML)
 
-    def calculer_angle(self):
-        if len(self.voisins) == 2:
-            x1,y1,z1 = ((self.voisins[0]).position)[0],((self.voisins[0]).position)[1],((self.voisins[0]).position)[2]
+    def _trouver_voisinsPOO(self):
+        parent = None
+        enfants = []
+        for articulation in sequence.postures[self.posture].articulations:
+            if trouver_parent(tronc,self.voisinsXML["P"])!=tronc:
+                if articulation.nom == self.voisinsXML["P"].attrib["Name"]:
+                    parent = articulation
+            else:
+                parent = None
+                
+            for enfantXML in self.voisinsXML["N"]:
+                if articulation.nom == enfantXML.attrib["Name"]:
+                    enfants.append(articulation)
+        return (parent,enfants)
+    voisinsPOO = property(_trouver_voisinsPOO)
+        
+
+
+    def _calculer_angle(self):
+        if len(self.voisinsPOO) == 2 and len(self.voisinsPOO[1])==1 :
+            x1,y1,z1 = ((self.voisinsPOO[0]).position)[0],((self.voisinsPOO[0]).position)[1],((self.voisinsPOO[0]).position)[2]
             x2,y2,z2 = (self.position)[0],(self.position)[1],(self.position)[2]
-            x3,y3,z3 = ((self.voisins[1]).position)[0],((self.voisins[1]).position)[1],((self.voisins[1]).position)[2]
+            x3,y3,z3 = ((self.voisinsPOO[1][0]).position)[0],((self.voisinsPOO[1][0]).position)[1],((self.voisinsPOO[1][0]).position)[2]
 
             # Calcule les vecteurs AB et BC
             AB = (x2 - x1, y2 - y1, z2 - z1)
@@ -60,10 +77,10 @@ class Articulation():
             return angle_deg
 
         else :
-            print("Extrémité!")
+            print("Extrémité ou main !")
             return None
         
-    angle = property(calculer_angle)
+    angle = property(_calculer_angle)
 
 
 
@@ -85,50 +102,79 @@ class Posture():
     numero = property(_lire_numero)
     articulations = property(_lire_articulations)
 
-    def _tracer_posture(self):
-            positions_articulations = [(self.articulations)[i].position for i in range(len(self.articulations))]
-
-            x = [point[0] for point in positions_articulations]
-            z = [point[1] for point in positions_articulations]
-            y = [point[2] for point in positions_articulations]
-
+    def _tracer_posture(self,instruction=0):
             # Création de la figure
             fig = plt.figure()
             ax = fig.add_subplot(111, projection='3d')
+            ax.set_xlim(0, 1)
+            ax.set_ylim(-0.6, 0.6) 
+            ax.set_zlim(0, 2)
+            
+            for articulation in self.articulations:
+                ax.scatter(articulation.position[0],articulation.position[2],articulation.position[1],c='r', marker='o') 
 
-            # Affichage des points en 3D
-            ax.scatter(x, y, z, c='r', marker='o')
+                if len(articulation.voisinsPOO[1])!=0:
+                    for enfant in articulation.voisinsPOO[1]:
+                        x1,x2 = enfant.position[0],articulation.position[0]
+                        y1,y2 = enfant.position[2],articulation.position[2] 
+                        z1,z2 = enfant.position[1],articulation.position[1]
+                        ax.scatter([x1,x2], [y1,y2], [z1,z2], c='r', marker='o')
+                        ax.plot([x1,x2], [y1,y2], [z1,z2], c='b')
+            dt = 1/1.2
+            if self.numero != 0 and self.numero != 57:
+                for articulation in self.articulations: 
+
+                    # Coordonnées 3D des points dans le temps
+                    temps = [(self.numero-1)*dt,self.numero*dt,(self.numero+1)*dt]
+                    x = [obtenir(articulation.nom,articulation.posture-1).position[0],obtenir(articulation.nom,articulation.posture).position[0],obtenir(articulation.nom,articulation.posture+1).position[0]]
+                    y = [obtenir(articulation.nom,articulation.posture-1).position[1],obtenir(articulation.nom,articulation.posture).position[1],obtenir(articulation.nom,articulation.posture+1).position[1]]
+                    z = [obtenir(articulation.nom,articulation.posture-1).position[2],obtenir(articulation.nom,articulation.posture).position[2],obtenir(articulation.nom,articulation.posture+1).position[2]]
+
+                    # Calcul des vecteurs déplacement
+                    deplacements = []
+                    for i in range(len(x) - 1):
+                        dx = x[i+1] - x[i]
+                        dy = y[i+1] - y[i]
+                        dz = z[i+1] - z[i]
+                        deplacements.append([dx, dy, dz])
+                    # Calcul des vecteurs vitesse moyenne de déplacement
+                    vitesse_moyenne_deplacement = np.mean(deplacements, axis=0)
+
+                    # Calcul des vecteurs accélérations
+                    accelerations = []
+                    for i in range(len(deplacements) - 1):
+                        dvx = deplacements[i+1][0] - deplacements[i][0]
+                        dvy = deplacements[i+1][1] - deplacements[i][1]
+                        dvz = deplacements[i+1][2] - deplacements[i][2]
+                        accelerations.append([dvx, dvy, dvz])
+                    # Calcul des vecteurs accélérations moyennes
+                    acceleration_moyenne = np.mean(accelerations, axis=0)
+
+                    # Tracé du vecteur vitesse
+                    origine = articulation.position
+                    if instruction in ("v","b"):
+                        ax.quiver(origine[0], origine[2], origine[1], vitesse_moyenne_deplacement[0], vitesse_moyenne_deplacement[2], vitesse_moyenne_deplacement[1], color='green')
+
+                    # Tracé du vecteur accélération
+                    if instruction in ("a","b"):
+                        ax.quiver(origine[0], origine[2], origine[1], acceleration_moyenne[0], acceleration_moyenne[2], acceleration_moyenne[1], color='magenta')
 
             # Configuration des axes
             ax.set_xlabel('X')
-            ax.set_ylabel('Z')
+            ax.set_ylabel('Z')               
             ax.set_zlabel('Y')
 
             # Orientation de la vue
-            ax.view_init(elev=6, azim=96)
-
+            ax.view_init(elev=15, azim=135)        
+           
             # Affichage de la figure
             plt.show()
 
     tracer = property(_tracer_posture)
 
-def creer_postures_list():
-    postures_list = []
-    _posturePOO = 0
-    c=0
-    for _postureXML in tronc:
-        articulations_list = []
-        for _articulationXML in _postureXML.iter("Joint"):
-            _articulationPOO = Articulation(_articulationXML.get("Name"),_articulationXML.get("Position"))
-            articulations_list.append(_articulationPOO)
-        _posturePOO = Posture(c,articulations_list)
-        postures_list.append(_posturePOO)
-        c+=1
-    return postures_list
-
-
 
 '______________Definition_classe_Séquence_____________'
+
 class Sequence():
     def __init__(self,tespostures):
         self._tespostures = list(tespostures) 
@@ -137,5 +183,38 @@ class Sequence():
         return self._tespostures
     postures = property(_lire_postures)
 
+
+
+'________________________Main_________________________'
+
+def trouver_parent(tronc, element):
+    for enfant in tronc:
+        if enfant is element:
+            return tronc
+        parent = trouver_parent(enfant, element)
+        if parent is not None:
+            return parent
+    return None
+
+def creer_postures_list():
+    postures_list = []
+    _posturePOO = 0
+    c=0
+    for _postureXML in tronc:
+        articulations_list = []
+        for _articulationXML in _postureXML.iter("Joint"):
+            above_voisin = trouver_parent(_postureXML,_articulationXML)
+            below_voisin = _articulationXML.findall("Joint")
+            _articulationPOO = Articulation(_articulationXML.get("Name"),_articulationXML.get("Position"),c,above_voisin,below_voisin)
+            articulations_list.append(_articulationPOO)
+        _posturePOO = Posture(c,articulations_list)
+        postures_list.append(_posturePOO)
+        c+=1
+    return postures_list
+
 sequence = Sequence(creer_postures_list())
     
+def obtenir(nom_articulation,numero_posture):
+    for articulation in sequence.postures[numero_posture].articulations:
+        if articulation.nom == nom_articulation:
+            return articulation
