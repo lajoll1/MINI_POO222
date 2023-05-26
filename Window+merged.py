5262dc80c5bbdf384af67c4b141e41f2f65a7fee
lajoll1 <nuel.thomas@gmail.com>
2023-05-26 02:48:14 +0200

    Update Window+merged.py

--- Window+merged.py
+++ Window+merged.py
@@ -864,7 +864,7 @@ def main():
             fichiers_charges= Chargement(root_txt_zone_1.get(), root_txt_zone_2.get())
             sequence = fichiers_charges.obtenir_sequence
            # print("Les chemins des fichiers d'imports sont: \n {} \n {}".format(root_txt_zone_1.get(), root_txt_zone_2.get()))
-            #afficher_onglets(fichiers_charges.obtenir_sequence,fichiers_charges.obtenir_regles)
+            afficher_onglets(fichiers_charges.obtenir_sequence,fichiers_charges.obtenir_regles)
             demo()    
         print("Fin de l'import")
 
